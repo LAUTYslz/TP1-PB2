9e0ad88e02f3b73a49416f2d98799a3a2acdb613
LAUTYslz <salazarlautaroalejandro@hotmail.com>
2023-09-23 17:12:45 -0300

    Agrego que se pueda agregar un alumno

--- test/Intraconsulta/TestCurso.java
+++ test/Intraconsulta/TestCurso.java
@@ -107,5 +107,39 @@ public class TestCurso {
 	}
 
 //	Universidad - Curso
+	@Test
+	public void queSePuedaAgregarUnAlumno() {
+		String nombre = "Unlam";
+		Universidad unlam = new Universidad(nombre);
+		nombre = "PB1";
+		Materia pb1 = new Materia(nombre);
+		unlam.registraMateria(pb1);
+		nombre = "PB2";
+		Materia materia = new Materia(nombre);
+		unlam.registraMateria(materia);
+		unlam.agregarCorrelativaAMateria(materia.getId(), pb1.getId());
+		Integer cantidadLugares = 40;
+		Aula aula = new Aula(cantidadLugares);
+		unlam.agregarAula(aula);
+		LocalDate fechaInicio = LocalDate.parse("2023-09-14");
+		LocalDate fechaFinalizacion = LocalDate.parse("2023-12-31");
+		LocalDate fechaInicioInscripcion = LocalDate.parse("2023-09-13");
+		LocalDate fechaFinalizacionInscripcion = LocalDate.parse("2023-09-15");
+		CicloLectivo nuevoCicloLectivo = new CicloLectivo(fechaInicio, fechaFinalizacion, fechaInicioInscripcion,
+				fechaFinalizacionInscripcion);
+		unlam.agregarCicloLectivo(nuevoCicloLectivo);
+		
+		String dia = "lunes";
+		String turno = "noche";
 
+		Curso nuevoCurso = new Curso(materia, dia, turno, nuevoCicloLectivo, aula);
+		Curso Duplicado = new Curso(materia, dia, turno, nuevoCicloLectivo, aula);
+
+		unlam.agregarCurso(nuevoCurso);
+
+		
+		assertFalse(unlam.agregarCurso(Duplicado));
+		assertEquals(unlam.getCursos().size(), 1);
+
+	}
 }
